aefe6b1aa8e03375436826c58af796eb8ba13c0c
hanjisungism00 <hanjisungism00.com>
2025-12-15 20:47:35 +0700

    commit message

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,22 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=RamenHut";
+            String username = "sa";
+            String password = "";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+}
