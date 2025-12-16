82280f69771932a1c55a6da4699e44ae215f00fc
llihn10 <nblinggg10@gmail.com>
2025-12-16 21:00:38 +0700

    edit

--- src/java/Controller/HomeServlet.java
+++ src/java/Controller/HomeServlet.java
@@ -1,29 +1,24 @@
 package Controller;
 
+import Model.FeedbackDAO;
+import Model.Feedback;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class HomeServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        FeedbackDAO dao = new FeedbackDAO();
+        List<Feedback> f = dao.getFeedback();
+        request.setAttribute("feedback", f);
+        request.getRequestDispatcher("home.jsp").forward(request, response);
     }
 
     @Override
